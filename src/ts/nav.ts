42514e157540d0bab5d0965184319e134d3bd388
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-10-03 17:19:01 +0200

    Feature : Mise à jour de l'activation automatique des lien sur le scroll (sa me satifait beaucoup plus)

--- src/ts/nav.ts
+++ src/ts/nav.ts
@@ -18,19 +18,24 @@ window.addEventListener('load', ()=>{
 
 function initAutoActive(navLinks: HTMLCollectionOf<Element>){
     let sections: Element[] = [];
+    //navLinks doit être dans le même ordre que les section dans la page sinon cela risque de posé problem
     for (const navLink of navLinks) {
         sections.push(getLikedElementFromLink(navLink))
     }
     window.onscroll = function () {
-        let current='';
+        let current='', prevHeight=0;
         for (const section of sections) {
             const sectionTop = section.getBoundingClientRect().top;
             const sectionHeight = section.clientHeight;
             //console.log(`Windows : ${window.scrollY} ${section.getAttribute("id")} : top${sectionTop} height${sectionHeight}`)
-            if (window.scrollY >= (sectionTop - sectionHeight / 5)){
+
+            //prevHeight permet de prendre en compte la taille de précédent composant pour évité de changer
+            // des l'instant ou la section est dépasser
+            if (window.scrollY >= (sectionTop - sectionHeight + prevHeight)){
                 const id = section.getAttribute('id')
                 if (id!==null) current = id
             }
+            prevHeight+=sectionHeight;
         }
         for (const navLink of navLinks) {
             if (navLink.getAttribute("href")?.substr(1,current.length)===current){
